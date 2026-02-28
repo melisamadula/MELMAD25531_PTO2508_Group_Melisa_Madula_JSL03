0802774529da731ba8f868e0940899a7a6a95d41
melisamadula <melisamadula.mm@gmail.com>
2026-03-01 01:35:13 +0200

    Removing unnecessary code and editing JavaScript code

--- scripts.js
+++ scripts.js
@@ -20,6 +20,7 @@ const initialTasks = [
   },
 ];
 
+// Function to add a new task to the initialTasks array
 const addTask = (title, description, status) => {
   const newTask = {
     id: initialTasks.length + 1, 
@@ -52,54 +53,16 @@ initialTasks.push(
   },
 );
 
+// Displaying All Tasks in the console
 console.log("All Tasks:", initialTasks);
-
-/*
-// Keep asking until the user enters a valid status for task
-while (addTask !== "done" && addTask !== "todo" && addTask !== "doing") {
-  alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
-  addTask = prompt("Enter task status (todo, doing, done):").toLowerCase();
-}
-
-if (addTask === "done" || addTask === "todo" || addTask === "doing") {
-  console.log("All Tasks:");
-  initialTasks.forEach(task => {
-    console.log(`- ${task.title}: ${task.description} (${task.status})`);
-  });
-}
-
-else if (addTask === "done") {
-  console.log("Completed Tasks:");
-  initialTasks.forEach(task => {
-    if (task.status === "done") {
-      console.log(`- ${task.title}: ${task.description}`);
-    }
-  });
-}
-*/
 
 // Displaying Alert Message for the last task added to the board
 const lastTask = initialTasks[initialTasks.length - 1];
 alert("These are your last tasks on your board, please check them on your console");
 
 // Filtering tasks based on their status and displaying them in the console
-/*const todoTasks = initialTasks.filter(task => task.status === "todo");
-const doingTasks = initialTasks.filter(task => task.status === "doing");*/
 const doneTasks = initialTasks.filter(task => task.status === "done");
 console.log("Completed Tasks:", doneTasks);
 
 
 
-/*
-//example to be deleted
-const doneTasks = initialTasks.filter(task => task.status === "done");
-console.log("Completed Tasks:");
-doneTasks.forEach(task => {
-  console.log(`- ${task.title}: ${task.description}`);
-});
-*/
-
-/* //from the previous code, to be deleted for just before alert message
-console.log("Last Task Added:");
-console.log(`- ${lastTask.title}: ${lastTask.description} (${lastTask.status})`);
-*/
